e64d06bbd0f30c850ee6080928dafbf2db9dcedc
Pranav Sarda <sardapranav1@gmail.com>
2020-11-28 17:44:45 +0530

    updated baseurl
    updated baseurl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
   url: 'https://sardapv.github.io',
-  baseUrl: '',
+  baseUrl: '/portfolio/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
