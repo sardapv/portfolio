00cd3316f1695eabf44913d6b83313e2ac0472a8
sardapv <sardapranav1@gmail.com>
2020-11-28 13:46:32 +0000

    Deploy website - based on d0abd4df72e5ac2497518d70cda2149902f65c76

---
+++ 6c435a8f.7c5e09d2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{77:function(o){o.exports=JSON.parse('{"allTagsPath":"/portfolio/blog/tags","slug":"hello","name":"hello","count":2,"permalink":"/portfolio/blog/tags/hello"}')}}]);
